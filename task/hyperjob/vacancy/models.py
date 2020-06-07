b3d2373679612f89fc301eece5865348bfa1ab22
Christopher-Hohberg <christopher.hohberg@gmx.de>
2020-06-07 20:41:22 +0200

    first stage complete
    added models for vacancy and resume

--- task/hyperjob/vacancy/models.py
+++ task/hyperjob/vacancy/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import django.contrib.auth.models
+
 
 # Create your models here.
+class Vacancy(models.Model):
+
+    class Meta:
+        app_label = "vacancy"
+
+    description = models.TextField(max_length=1024)
+    author = models.ForeignKey(django.contrib.auth.models.User, on_delete=models.CASCADE)
+
+
+class Resume(models.Model):
+
+    class Meta:
+        app_label = "resume"
+
+    description = models.TextField(max_length=1024)
+    author = models.ForeignKey(django.contrib.auth.models.User, on_delete=models.CASCADE)
